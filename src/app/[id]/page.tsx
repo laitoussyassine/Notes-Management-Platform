30c8b737e52fde1e32c18ae26b4d3a005bd080c0
laitouss-yassine <yassinelaitouss2380@gmail.com>
2024-02-27 13:05:58 +0100

    first initial project commit

---
+++ src/app/[id]/page.tsx
@@ -0,0 +1,15 @@
+"use client"
+import { useParams, useRouter } from 'next/navigation'
+
+const EditNoteId = () => {
+    const params = useParams()
+    const router = useRouter()
+  return (
+    <>
+        <h1>edit note id :{params.id}</h1>
+        <div onClick={() => router.push('/')}>go to notes page</div>
+    </>
+  )
+}
+
+export default EditNoteId
